5a2c38f0dc1e9e8e8a75fd01050849a5d6e3647a
adhamawad06@gmail.com <adhamawad06@gmail.com>
2023-06-17 00:08:05 +0200

    :poop: Add food search input and cards showing informations

--- src/pages/FoodDatabase.js
+++ src/pages/FoodDatabase.js
@@ -1,9 +1,42 @@
-import * as React from "react";
-import { Button } from "react-native-paper";
-import Constants from 'expo-constants';
+import React, { useState } from "react";
+import { Button, Card, Text, TextInput } from "react-native-paper";
+import { ScrollView, View } from "react-native";
+import { fetchFoodData } from "../services/apiService";
 
 const FoodDatabase = () => {
-  return <Button onPress={() => console.log( Constants.expoConfig.extra.apiAppKey )}> Touch me Food DB </Button>;
+  const [searchQuery, setSearchQuery] = useState('');
+  const [foodData, setFoodData] = useState(null);
+
+  const handlePress = async () => {
+    try {
+      const data = await fetchFoodData(searchQuery);
+      setFoodData(data["hints"]);
+    } catch (error) {
+      console.error("Error searching for food:", error);
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        mode="outlined"
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+        placeholder="Enter food item"
+      />
+      <Button onPress={handlePress}>Touch me Food DB</Button>
+      <ScrollView>
+        {foodData && foodData.map((data, index) => (
+          <Card mode="outlined">
+            <Card.Content>
+              <Text variant="titleLarge">{data.food.label}</Text>
+            </Card.Content>
+            <Card.Cover source={{ uri: data.food.image }} />
+          </Card>
+        ))}
+      </ScrollView>
+    </View>
+  );
 };
 
 export default FoodDatabase;
