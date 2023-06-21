7fd89c3ff104c3ecbc3b6403bf8625f4166d0fb0
adhamawad06@gmail.com <adhamawad06@gmail.com>
2023-06-21 21:45:37 +0200

    :recycle: Add a callback to the NumberInput

--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { TextInput } from "react-native-paper";
 
-const NumberInput = ({ label }) => {
+const NumberInput = ({ label, onChange }) => {
   const [number, setNumber] = useState("100");
+
+  const handleNumberChange = (value) => {
+    setNumber(value);
+    onChange(value);
+  };
 
   return (
     <TextInput
       label={label}
       value={number}
-      onChangeText={setNumber}
+      onChangeText={handleNumberChange}
       keyboardType="numeric"
       mode="outlined"
     />
